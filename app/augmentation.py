613499b2fb50f206614f0e62c0144b5ef15dec52
serhii dolhopolov <serhiidolhopolovcoder@gmail.com>
2025-05-17 09:56:14 +0300

    changes

--- app/augmentation.py
+++ app/augmentation.py
@@ -4,8 +4,8 @@ from torchvision.transforms import v2
 
 class AugmentationV1:
     size = (128, 128)
-    mean_normalize = [0.485, 0.456, 0.406]
-    std_normalize = [0.229, 0.224, 0.225]
+    mean = [0.5, 0.5, 0.5]
+    std = [0.5, 0.5, 0.5]
         
     def get_train(self):
         compose_list = [
@@ -13,7 +13,7 @@ class AugmentationV1:
             v2.TrivialAugmentWide(num_magnitude_bins=31),
             v2.ToImage(),
             v2.ToDtype(torch.float32, scale=True),
-            v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            v2.Normalize(mean=self.mean, std=self.std),
         ]        
         augment = v2.Compose(compose_list)
         return augment
@@ -23,8 +23,33 @@ class AugmentationV1:
             v2.Resize(self.size),
             v2.ToImage(),
             v2.ToDtype(torch.float32, scale=True),
-            v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            v2.Normalize(mean=self.mean, std=self.std),
         ]
         augment = v2.Compose(compose_list)
         return augment
     
+class AugmentationTransferLearning:
+    size = (224, 224)
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    
+    def get_train(self):
+        compose_list = [
+            v2.Resize(self.size),
+            v2.TrivialAugmentWide(num_magnitude_bins=31),
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(mean=self.mean, std=self.std),
+        ]        
+        augment = v2.Compose(compose_list)
+        return augment
+    
+    def get_test(self):
+        compose_list = [
+            v2.Resize(self.size),
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(mean=self.mean, std=self.std),
+        ]
+        augment = v2.Compose(compose_list)
+        return augment
