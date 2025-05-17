f260c2f9368c1897adb07408c5090455897bf07c
serhii dolhopolov <serhiidolhopolovcoder@gmail.com>
2025-05-17 08:45:10 +0300

    changes

--- app/augmentation.py
+++ app/augmentation.py
@@ -3,15 +3,41 @@ from torchvision.transforms import v2
 
 
 class AugmentationV1:
-    size = (128, 128)
+    size = (64, 64)
     mean_normalize = [0.485, 0.456, 0.406]
     std_normalize = [0.229, 0.224, 0.225]
         
     def get_train(self):
         compose_list = [
             v2.Resize(self.size),
-            #v2.TrivialAugmentWide(num_magnitude_bins=31),
-            v2.RandomHorizontalFlip(),
+            v2.TrivialAugmentWide(num_magnitude_bins=31),
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        ]        
+        augment = v2.Compose(compose_list)
+        return augment
+        
+    def get_test(self):
+        compose_list = [
+            v2.Resize(self.size),
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        ]
+        augment = v2.Compose(compose_list)
+        return augment
+    
+    
+class AugmentationV2:
+    size = (224, 224)
+    mean_normalize = [0.485, 0.456, 0.406]
+    std_normalize = [0.229, 0.224, 0.225]
+        
+    def get_train(self):
+        compose_list = [
+            v2.Resize(self.size),
+            v2.TrivialAugmentWide(num_magnitude_bins=31),
             v2.ToImage(),
             v2.ToDtype(torch.float32, scale=True),
             v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
